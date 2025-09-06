099ca2a664f6ff7a94f7186b529f6e9cf65e2f99
Nikhil Goud <padalan@gmail.com>
2025-09-06 16:38:12 +0530

    Add deployment_mode variable and conditional resource creation for hobby mode

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,9 +1,14 @@
 output "sns_topic_arn" {
   description = "ARN of the SNS topic for billing alerts"
-  value       = module.billing.sns_topic_arn
+  value       = length(module.billing) > 0 ? module.billing[0].sns_topic_arn : null
 }
 
 output "billing_alarms" {
   description = "List of billing alarm names"
-  value       = module.billing.billing_alarm_names
+  value       = length(module.billing) > 0 ? module.billing[0].billing_alarm_names : []
+}
+
+output "deployment_mode" {
+  description = "Current deployment mode"
+  value       = var.deployment_mode
 }
